cf44e3e95c9e9504bf1918beb87294db10c722ef
ilapekka7@gmail.com <ilapekka7@gmail.com>
2025-12-27 21:00:29 +0300

    Добавьте файлы проекта.

---
+++ Convertor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Convertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tb1 = new TextBox();
+            convert = new Button();
+            cb1 = new ComboBox();
+            lb1 = new LinkLabel();
+            lb2 = new Label();
+            btnUpdate = new Button();
+            SuspendLayout();
+            // 
+            // tb1
+            // 
+            tb1.Location = new Point(75, 40);
+            tb1.Name = "tb1";
+            tb1.Size = new Size(75, 23);
+            tb1.TabIndex = 1;
+            // 
+            // convert
+            // 
+            convert.Location = new Point(75, 95);
+            convert.Name = "convert";
+            convert.Size = new Size(117, 23);
+            convert.TabIndex = 3;
+            convert.Text = "конвертировать";
+            convert.UseVisualStyleBackColor = true;
+            convert.Click += convert_Click;
+            // 
+            // cb1
+            // 
+            cb1.FormattingEnabled = true;
+            cb1.Items.AddRange(new object[] { "доллары в рубли", "рубли в доллары" });
+            cb1.Location = new Point(237, 96);
+            cb1.Name = "cb1";
+            cb1.RightToLeft = RightToLeft.No;
+            cb1.Size = new Size(121, 23);
+            cb1.TabIndex = 4;
+            cb1.SelectedIndexChanged += cb1_SelectedIndexChanged;
+            // 
+            // lb1
+            // 
+            lb1.AutoSize = true;
+            lb1.LinkColor = Color.Black;
+            lb1.Location = new Point(90, 154);
+            lb1.Name = "lb1";
+            lb1.Size = new Size(41, 15);
+            lb1.TabIndex = 5;
+            lb1.TabStop = true;
+            lb1.Text = "вывод";
+            // 
+            // lb2
+            // 
+            lb2.AutoSize = true;
+            lb2.Location = new Point(237, 48);
+            lb2.Name = "lb2";
+            lb2.Size = new Size(38, 15);
+            lb2.TabIndex = 6;
+            lb2.Text = "label1";
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(237, 146);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(169, 23);
+            btnUpdate.TabIndex = 7;
+            btnUpdate.Text = "button1";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUpdate);
+            Controls.Add(lb2);
+            Controls.Add(lb1);
+            Controls.Add(cb1);
+            Controls.Add(convert);
+            Controls.Add(tb1);
+            Name = "Form1";
+            Text = "Form1";
+            TransparencyKey = Color.Gainsboro;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox tb1;
+        private Button convert;
+        private ComboBox cb1;
+        private LinkLabel lb1;
+        private Label lb2;
+        private Button btnUpdate;
+    }
+}
---
+++ Convertor/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.Json;
+
+
+namespace Convertor
+{
+    public partial class Form1 : Form
+    {
+        double price = 78;
+        public Form1()
+        {
+            InitializeComponent();
+            cb1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cb1.Items.Clear();
+            cb1.Items.Add("доллары в рубли");
+            cb1.Items.Add("рубли в доллары");
+            cb1.SelectedIndex = 1;
+            convert.Text = "конвертировать";
+            lb2.Text = $"{price} рублей за доллар";
+            UpdateRateFromCbr();
+            btnUpdate.Text = "Обновить";
+        }
+
+
+        private async void UpdateRateFromCbr()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var json = await client.GetStringAsync("https://www.cbr-xml-daily.ru/daily_json.js");
+
+                // Парсим JSON
+                using var document = JsonDocument.Parse(json);
+                var usdValue = document.RootElement
+                    .GetProperty("Valute")
+                    .GetProperty("USD")
+                    .GetProperty("Value")
+                    .GetDouble(); // ← Реальный курс!
+
+                price = usdValue;
+                lb2.Text = $"{price:F2} рублей за доллар";
+
+
+            }
+            catch
+            {
+                MessageBox.Show(" Ошибка загрузки. Используется курс по умолчанию.");
+            }
+        }
+
+
+
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+
+
+
+            double result = 0;
+
+            if (!double.TryParse(tb1.Text, out double a) || a <= 0)
+            {
+                MessageBox.Show("Введите корректную сумму!");
+                return;
+            }
+
+            if (cb1.Text == "доллары в рубли")
+                result = a * price;
+
+            if (cb1.Text == "рубли в доллары")
+                result = a / price;
+
+            lb1.Text = result.ToString("F2");
+        }
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            btnUpdate.Text = "Курс обновлён";
+            UpdateRateFromCbr();
+            
+        }
+    }
+}
